1df316bd1d8a697fc610e2588ccefad1703982c5
Vincent Palomo <vincentpalomo@yahoo.com>
2023-01-09 23:43:09 -0600

    finished the main index and all the data is being logged

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // main index file, currently being run by nodemon
+
+const { client } = require('./db');
+
+async function getPets() {
+  const { rows } = await client.query(`
+ SELECT *
+ FROM pets;
+ `);
+  console.log(rows);
+}
+
+async function getOwners() {
+  const { rows } = await client.query(`
+  SELECT *
+  FROM owners;
+  `);
+  console.log(rows);
+}
+
+getPets();
+
+getOwners();
